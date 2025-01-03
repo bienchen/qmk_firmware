57951efcc5ab6aff8c83840a8770a78e3d0ba3b4
B13nch3n <b13nch3n-gitea@theb-si.de>
2025-01-03 16:21:04 +0100

    Determine positions with/ without LEDs by function pointer so different keyboards can use the functionality

--- users/b13nch3n/rgb_layer.h
+++ users/b13nch3n/rgb_layer.h
@@ -5,7 +5,7 @@
 
 #include "quantum.h"
 
-void rgb_matrix_set_non_passthrough_color(const uint16_t[][MATRIX_ROWS][MATRIX_COLS], uint8_t, ...);
+void rgb_matrix_set_non_passthrough_color(const uint16_t[][MATRIX_ROWS][MATRIX_COLS], bool (*)(uint8_t, uint8_t), uint8_t, ...);
 
 void rgb_matrix_set_color_hsv(uint8_t, uint8_t, uint8_t, uint8_t);
 
